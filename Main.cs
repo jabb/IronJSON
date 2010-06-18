f03a207d495f52addb0ba5c714b71c94f5a40505
Michael Patraw <grue@zork.(none)>
2010-06-18 16:08:46 -0700

    Edited README.

--- Main.cs
+++ Main.cs
@@ -1,40 +1,25 @@
 using System;
 using System.IO;
+using IronJSON;
 
-namespace IronJSON
+namespace Example
 {
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			Test("test1.json");
-			Test("test2.json");
-			Test("test3.json");
-			Test("test4.json");
-			Test("test5.json");
-		}
-		
-		static void Test(string filename)
-		{
-			Console.WriteLine("\n\nTesting: " + filename + "...");
-			try
-			{
-				JSONManager json = new JSONManager(filename);
-				json.Save(filename + "f.json");
-			}
-			catch (JSONException ex)
-			{
-				Console.WriteLine("Error: " + ex.Message);
-				Console.WriteLine("Check error message for correctness.");
-				return;
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Error: " + ex.Message);
-				Console.WriteLine("Test failed!");
-				return;
-			}
-			Console.WriteLine("Test passed!");
+			JSONManager json = new JSONManager();
+			
+			json.SetToInteger("anint", 10);
+			
+			// Create a JSON object and "CD" to it so we can add stuff.
+			json.SetToObject("anobject");
+			json.Cd(JSONManager.Path.Relative, "anobject");
+			
+			// Add stuff to it.
+			json.SetToString("astring", "Hello, world!");
+			
+			json.Save("example.json");
 		}
 	}
 }
